f4d7ec73ae2d2939c1a331d6e2262dc06e800185
Mr.chu <chuhongkyu@gmail.com>
2023-01-01 22:19:29 +0900

    Add Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import HeadComponent from 'components/HeadComponent'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
@@ -32,6 +33,7 @@ export default function Intro() {
 
   return (
     <AnimatePresence>
+      <HeadComponent/>
       <div id={styles.intro}>
         <motion.div variants={Variants} initial="initial" animate="animate" exit="exit" className={styles.intro_img}>
         </motion.div>
